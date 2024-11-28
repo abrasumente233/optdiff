9feeb05a42b77a0f1b155878b74d843a4c5f3eda
abrasumente <why-excuse-quail@duck.com>
2024-11-28 18:54:32 +0800

    input via arg or stdin

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,40 @@
 use pager::Pager;
 use std::error::Error;
-use std::io::Write;
+use std::io::{self, Read, Write};
 use std::process::Command;
 use tempfile::NamedTempFile;
-
 mod optpipeline;
 
+fn read_input() -> Result<String, io::Error> {
+    let mut args = std::env::args().skip(1);
+    match args.next() {
+        Some(path) => std::fs::read_to_string(path),
+        None => {
+            let mut buffer = String::new();
+            io::stdin().read_to_string(&mut buffer)?;
+            Ok(buffer)
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    Pager::with_default_pager("less -R").setup();
-    let dump = std::fs::read_to_string("dumpfuck.txt").unwrap();
+    let dump = read_input().map_err(|e| format!("Failed to read input: {}", e))?;
     let result = optpipeline::process(&dump);
-    //println!("{:#?}", result);
+
+    Pager::with_default_pager("less -R").setup();
 
     let mut before = NamedTempFile::new()?;
     writeln!(before, "hello")?;
-
     let mut after = NamedTempFile::new()?;
     writeln!(after, "hello world")?;
 
     for pass in &result["a"] {
         let mut old = NamedTempFile::new()?;
         write!(old, "{}", pass.before)?;
-
         let mut new = NamedTempFile::new()?;
         write!(new, "{}", pass.after)?;
 
-        // Reference: https://git-scm.com/docs/git#Documentation/git.txt-codeGITEXTERNALDIFFcode
-        Command::new("difft")
+        let status = Command::new("difft")
             .arg("--color")
             .arg("always")
             .arg(&pass.name)
@@ -39,11 +47,13 @@ fn main() -> Result<(), Box<dyn Error>> {
             .env("GIT_DIFF_PATH_TOTAL", "15")
             .env("GIT_DIFF_PATH_COUNTER", "13")
             .status()
-            .expect("failed to execute process");
+            .map_err(|e| format!("Failed to execute difft: {}", e))?;
+
+        if !status.success() {
+            return Err("difft command failed".into());
+        }
     }
 
-    // Call diff with environment variable PAGER set to "less"
-
-    //println!("{}", String::from_utf8_lossy(&diff.stdout));
     Ok(())
 }
+
